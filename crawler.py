e68541682bc998be3be872e26110af0fade68813
Gergo Pap <53934464+Gr3gPr1est@users.noreply.github.com>
2020-01-08 11:45:44 +0100

    Add files via upload

---
+++ crawler.py
@@ -0,0 +1,19 @@
+import io
+import hashlib
+import sys
+from itertools import product
+
+def my_function():
+    array = ["!","%","/","=","(",")"]
+    a = 0
+    while (a!=5):
+        print word+array[a]*b
+        a = a + 1
+
+with open("list2.txt") as f:
+    for line in f:
+        for word in line.split():
+            b = 0
+            while (b != 10):
+                my_function()
+                b = b + 1
